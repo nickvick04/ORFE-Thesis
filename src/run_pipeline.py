42012e2c5a6ab9de9295e617c0077d0f439ae116
Nicholas Vickery <nv9344@princeton.edu>
2026-02-16 15:23:54 -0500

    new batching pipeline function

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -4,15 +4,18 @@
 # ----------------------------------------------------------------------------------------
 # imports
 import os
-from data_preprocessing import corpus_to_df, filter_df
+from data_preprocessing import corpus_to_df, corpus_to_df_batches, filter_df
 from lexical_analysis_functions import compute_lexical_vals
 from syntactic_analysis_functions import compute_syntactic_vals
 from visualization import *
 from convokit import Corpus
 
+BATCH_SIZE = 5000
+
 def run_full_pipeline_cnvkt(corpus_dir: str):
     '''Runs full preprocessing and analysis pipeline on a single Convokit
     corpus and writes a CSV to the corpus' parent Variation folder'''
+
     corpus_name = os.path.basename(corpus_dir)
     print(f"Processing corpus: {corpus_name}")
 
@@ -36,6 +39,47 @@ def run_full_pipeline_cnvkt(corpus_dir: str):
     df.to_csv(output_path)
     print(f"Saved -> {output_path}\n")
 
+def run_full_pipeline_cnvkt_batches(corpus_dir: str, batch_size=5000):
+    '''Runs full preprocessing and analysis pipeline on a single Convokit
+    corpus and writes a CSV to the corpus' parent Variation folder in batches
+    so as to reduce the memory capacity demanded of the cluster.'''
+
+    # extract corpus
+    corpus_name = os.path.basename(corpus_dir)
+    print(f"Processing corpus: {corpus_name}")
+
+    # get path to csv next to corpus
+    output_dir = os.path.dirname(corpus_dir)
+    output_path = os.path.join(output_dir, f"{corpus_name}_df.csv")
+
+    # load corpus
+    print(f"Loading corpus: {corpus_name}")
+    corpus = Corpus(corpus_dir)
+
+    # boolean and index to track
+    first_batch = True
+    i = 0
+    print(f"Processing {corpus_name} in batches...")
+    print(f"Currently processing batch: {i}")
+
+    # iterate through batches, writing out results incrementally
+    for df_batch in corpus_to_df_batches(corpus, batch_size=BATCH_SIZE):
+        
+        df_batch = filter_df(df_batch)
+
+        print(f"Analyzing corpus batch: {corpus_name}")
+        df_batch = compute_lexical_vals(df_batch)
+        df_batch = compute_syntactic_vals(df_batch)
+
+        df_batch.set_index('timestamp', inplace=True)
+        # write to new file if first batch, o/w append to existing file
+        df_batch.to_csv(output_path, mode="w" if first_batch else "a", header=first_batch)
+
+        # udpate boolean and index
+        first_batch = False
+        i += 1
+        # delete explicitly to save storage
+        del df_batch
 
 def run_all_corpora_cnvkt(convokit_root: str):
     '''Runs pipeline for all corpora under Convokit root directory.'''
